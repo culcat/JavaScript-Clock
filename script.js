bd80c8af8a4ec40162194aefa7d43e26e858f7d9
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2022-08-18 15:33:42 +0300

    delete pm/am systeme

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ function calculateTime(){
     var dayNumber = date.getDate();
     var hour = date.getHours();
     var minute = date.getMinutes();
-    var pmam = hour >= 12 ? 'PM' : 'AM';
+   // var pmam = hour >= 12 ? 'PM' : 'AM';
     var dayNames =  ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-    hour = hour % 12;
-    hour = hour ? 12 : '12';
-    hour = hour < 10 ? '0' + hour : hour;
-    minute = minute < 10 ? '0' + minute : minute;
+    //hour = hour % 12;
+    //hour = hour ? 12 : '12';
+    //hour = hour < 10 ? '0' + hour : hour;
+    //minute = minute < 10 ? '0' + minute : minute;
 
     document.getElementById("day").innerHTML = dayNames[dayNumber];
     document.getElementById("hour").innerHTML = hour;
     document.getElementById("minute").innerHTML = minute;
-    document.getElementById("pmam").innerHTML = pmam;
+    //document.getElementById("pmam").innerHTML = pmam;
 
     setTimeout(calculateTime, 200);
 console.log(calculateTime)
