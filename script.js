6edf758243fca8d2989d5f3ef2d028f4d60ea3e9
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2022-09-17 08:07:27 +0300

    update interface

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ window.addEventListener('load', calculateTime)
 function calculateTime(){
     var date = new Date();
     var dayNumber = date.getDate();
+    var dayNames = date.getDate();
     var hour = date.getHours();
     var minute = date.getMinutes();
-   // var pmam = hour >= 12 ? 'PM' : 'AM';
+    //var pmam = hour >= 12 ? 'PM' : 'AM';
     var dayNames =  ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-    //hour = hour % 12;
-    //hour = hour ? 12 : '12';
-    //hour = hour < 10 ? '0' + hour : hour;
-    //minute = minute < 10 ? '0' + minute : minute;
-
-    document.getElementById("day").innerHTML = dayNames[dayNumber];
+    // hour = hour % 12;
+    // hour = hour ? 12 : '12';
+    // hour = hour < 10 ? '0' + hour : hour;
+    // minute = minute < 10 ? '0' + minute : minute;
+    
+    document.getElementById("dayToday").innerHTML = dayNames[dayNumber];
+    document.getElementById("day").innerHTML = dayNumber;
     document.getElementById("hour").innerHTML = hour;
     document.getElementById("minute").innerHTML = minute;
     //document.getElementById("pmam").innerHTML = pmam;
@@ -20,3 +22,9 @@ function calculateTime(){
     setTimeout(calculateTime, 200);
 console.log(calculateTime)
 }
+
+const back = document.querySelector("body");
+back.style.background = "black"; 
+
+const font = document.querySelector("h1");
+back.style.color = "white"; 
