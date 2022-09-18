a46eb38d1ecdb9a83fb5e70b0acd1aff45d51ae3
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2022-09-18 11:44:39 +0300

    fixed bug

--- script.js
+++ script.js
@@ -2,25 +2,33 @@ window.addEventListener('load', calculateTime)
 function calculateTime(){
     var date = new Date();
     var dayNumber = date.getDate();
-    var dayNames = date.getDate();
+    //var dayNames = date.getUTCMonth();
     var hour = date.getHours();
     var minute = date.getMinutes();
+    const month = ["Января","Февраля","Марта","Апреля","Мая","Июня","Июля","Августа","Сентября","Октября","Ноября","Декабря"];
+    const d = new Date();
+    let name = month[d.getMonth()];
     //var pmam = hour >= 12 ? 'PM' : 'AM';
-    var dayNames =  ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    //var dayNames =  ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
     // hour = hour % 12;
     // hour = hour ? 12 : '12';
     // hour = hour < 10 ? '0' + hour : hour;
     // minute = minute < 10 ? '0' + minute : minute;
     
-    document.getElementById("dayToday").innerHTML = dayNames[dayNumber];
+   // document.getElementById("dayToday").innerHTML = dayNames;
     document.getElementById("day").innerHTML = dayNumber;
     document.getElementById("hour").innerHTML = hour;
     document.getElementById("minute").innerHTML = minute;
     //document.getElementById("pmam").innerHTML = pmam;
+    document.getElementById("dayToday").innerHTML = name;
+
+
 
     setTimeout(calculateTime, 200);
-console.log(calculateTime)
+    console.log(calculateTime);
+
+
 }
 
 const back = document.querySelector("body");
